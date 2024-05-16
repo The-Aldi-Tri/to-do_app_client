1e1a2741322a5be5f0d7b35fbce5d6126c46c3df
The-Aldi-Tri <the.aldi.tri@gmail.com>
2024-05-16 19:25:54 +0700

    Make more responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Container } from "@mui/material";
 import "./App.css";
 import MuiTable from "./components/MuiTable";
 import MuiForm from "./components/MuiForm";
@@ -10,8 +9,20 @@ const dummy = [
     "Task 1",
     "lorem ipsum dolor sit amet",
     new Date("2024-05-15T09:00:00")
-  ), // Empty description
-  createData("Task 2", "A short description", new Date("2024-05-16T10:30:00")), // Short description
+  ),
+  createData("Task 2", "A short description", new Date("2024-05-16T10:30:00")),
+  createData(
+    "Task 3",
+    "A longer description for Task 3",
+    new Date("2024-05-17T13:45:00")
+  ),
+  createData("Task 4", "", new Date("2024-05-18T15:15:00")),
+  createData(
+    "Task 5",
+    "Another short description",
+    new Date("2024-05-19T17:00:00")
+  ),
+  createData("Task 6", "Description 6", new Date("2024-05-20T08:00:00")),
 ];
 
 const App = () => {
@@ -22,13 +33,13 @@ const App = () => {
   };
 
   return (
-    <Container maxWidth="md">
+    <div className="container">
       <br />
       <MuiForm sharedData={sharedData} handleSharedData={handleSharedData} />
       <br />
       <MuiTable sharedData={sharedData} handleSharedData={handleSharedData} />
       <br />
-    </Container>
+    </div>
   );
 };
 
