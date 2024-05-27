8a3383ee0bf080de42655080256942a62c7b4ffe
The-Aldi-Tri <the.aldi.tri@gmail.com>
2024-05-27 20:02:54 +0700

    Add loading animation

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/authContext";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuth();
 
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "75vh", // Full viewport height
+  };
+
+  const textStyle = {
+    marginTop: "20px",
+    fontSize: "1.5rem",
+  };
+
   if (isLoading) {
-    return <div>Loading...</div>; // Or any loading indicator you prefer
+    return (
+      <div style={containerStyle}>
+        <CircularProgress style={{ color: "#61dafb" }} />
+        <div style={textStyle}>Loading . . .</div>
+      </div>
+    );
   }
 
   return isAuthenticated ? children : <Navigate to="/login-signup" />;
